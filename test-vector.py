e81c3f98979e433e07a904cbe1de4f316f68c7e0
siv2r <siv2ram@gmail.com>
2024-03-04 19:57:58 +0530

    test_vectors: put them into two csv files

--- test-vector.py
+++ test-vector.py
@@ -13,6 +13,9 @@ def has_square_y(P):
 def point_to_bytes(P):
     return (bytes_from_int(P[0]), bytes_from_int(P[1]))
 
+#
+# `schnorr_presig_sign` test vectors
+#
 def vector0():
     seckey = bytes_from_int(3)
     msg = bytes_from_int(0)
@@ -36,7 +39,7 @@ def vector0():
     P = point_mul(G, x)
     assert(y(P) % 2 == 0)
 
-    return (seckey, pubkey, aux_rand, msg, T, t, sig, sig64, None, "TRUE", "seckey and secadaptor are small")
+    return (seckey, pubkey, aux_rand, msg, T, sig, "TRUE", "seckey and secadaptor are small")
 
 def vector1():
     seckey = bytes_from_int(0x0B432B2677937381AEF05BB02A66ECD012773062CF3FA2549E44F58ED2401710)
@@ -58,7 +61,7 @@ def vector1():
 
     sig = schnorr_presig_sign(msg, seckey, aux_rand, T)
     sig64 = schnorr_adapt(sig, t)
-    return (seckey, pubkey_gen(seckey), aux_rand, msg, T, t, sig, sig64, None, "TRUE", "test fails if msg is reduced modulo p or n")
+    return (seckey, pubkey_gen(seckey), aux_rand, msg, T, sig, "TRUE", "test fails if msg is reduced modulo p or n")
 
 def vector2():
     seckey = bytes_from_int(0xC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B14E5C9)
@@ -74,13 +77,16 @@ def vector2():
 
     sig = schnorr_presig_sign(msg, seckey, aux_rand, T)
     sig64 = schnorr_adapt(sig, t)
-    return (seckey, pubkey_gen(seckey), aux_rand, msg, T, t, sig, sig64, None, "TRUE", None)
+    return (seckey, pubkey_gen(seckey), aux_rand, msg, T, sig, "TRUE", None)
 
 default_seckey = bytes_from_int(0xB7E151628AED2A6ABF7158809CF4F3C762E7160F38B4DA56A784D9045190CFEF)
 default_msg = bytes_from_int(0x243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89)
 default_aux_rand = bytes_from_int(0xC87AA53824B4D7AE2EB035A2B5BBBCCC080E76CDC6D1692C4B0B62D798E6D906)
 default_T = compress_point(point_mul(G, 5))
 
+#
+# `schnorr_extract_adaptor` test vectors
+#
 def vector3():
     # This creates a dummy signature that doesn't have anything to do with the
     # public key.
@@ -91,14 +97,14 @@ def vector3():
     pubkey = bytes_from_int(0xEEFDEA4CDB677750A420FEE807EACF21EB9898AE79B9768766E4FAA04A2D4A34)
     assert(lift_x(int_from_bytes(pubkey)) is None)
 
-    return (None, pubkey, None, msg, default_T, None, sig, None, "Verification", "FALSE", "Verification test: public key not on the curve")
+    return (None, pubkey, None, msg, default_T, sig, "FALSE", "Verification test: public key not on the curve")
 
 def vector4():
     seckey = default_seckey
     msg = int_from_bytes(default_msg)
     neg_msg = bytes_from_int(n - msg)
     sig = schnorr_presig_sign(neg_msg, seckey, default_aux_rand, default_T)
-    return (None, pubkey_gen(seckey), None, bytes_from_int(msg), default_T, None, sig, None, "Verification", "FALSE", "Verification test: negated message")
+    return (None, pubkey_gen(seckey), None, bytes_from_int(msg), default_T, sig, "FALSE", "Verification test: negated message")
 
 def vector5():
     seckey = default_seckey
@@ -109,7 +115,7 @@ def vector5():
     sig = schnorr_presig_sign(msg, seckey, default_aux_rand, T)
     sig = sig[0:33] + bytes_from_int(n - int_from_bytes(sig[33:65]))
 
-    return (None, pubkey_gen(seckey), None, msg, T, None, sig, None, "Verification", "FALSE", "Verification test: negated s' value")
+    return (None, pubkey_gen(seckey), None, msg, T, sig, "FALSE", "Verification test: negated s' value")
 
 def vector6():
     seckey = default_seckey
@@ -117,7 +123,7 @@ def vector6():
     sig = schnorr_presig_sign(msg, seckey, default_aux_rand, default_T)
     negated_parity = b"\x02" if sig[0] == 3 else b"\x03"
     sig = negated_parity + sig[1:]
-    return (None, pubkey_gen(seckey), None, msg, default_T, None, sig, None, "Verification", "FALSE", "Verification test: negated R' value")
+    return (None, pubkey_gen(seckey), None, msg, default_T, sig, "FALSE", "Verification test: negated R' value")
 
 def vector7():
     seckey = default_seckey
@@ -129,15 +135,18 @@ def vector7():
     R0 = bytes_from_int(0xEEFDEA4CDB677750A420FEE807EACF21EB9898AE79B9768766E4FAA04A2D4A34)
     assert(lift_x(int_from_bytes(R0)) is None)
     sig = sig[0:1] + R0 + sig[33:65]
-    return (None, pubkey_gen(seckey), None, msg, default_T, None, sig, None, "Verification", "FALSE", "Verification test: R' not on the curve")
+    return (None, pubkey_gen(seckey), None, msg, default_T, sig, "FALSE", "Verification test: R' not on the curve")
 
+#
+# `schnorr_adapt` test vectors
+#
 def vector8():
     t = 75822971893802771970555672493312142286329633334319705105105405901505850897681
     T = compress_point(point_mul(G, t))
     t = bytes_from_int(t)
     sig = schnorr_presig_sign(default_msg, default_seckey, default_aux_rand, T)
     sig64 = schnorr_adapt(sig, t)
-    return (None, None, None, None, T, t, sig, sig64, "Adaptor extraction and Adapting", "TRUE", "Adaptor extraction and Adapting test: adaptor t is a large scalar")
+    return (t, sig, sig64, "TRUE", "Adaptor extraction and Adapting test: adaptor t is a large scalar")
 
 def vector9():
     t = 102737890522302903489175669941446530087577081774431208355392319064688794746007
@@ -147,7 +156,7 @@ def vector9():
     sig64 = schnorr_adapt(sig, t)
     s = int_from_bytes(sig[33:65])
     sig = sig[0:33] + bytes_from_int(n - s)
-    return (None, None, None, None, T, t, sig, sig64, "Adaptor extraction and Adapting", "FALSE", "Adaptor extraction and Adapting test: negated s' value")
+    return (t, sig, sig64, "FALSE", "Adaptor extraction and Adapting test: negated s' value")
 
 def vector10():
     t = 102737890522302903489175669941446530087577081774431208355392319064688794746007
@@ -157,7 +166,7 @@ def vector10():
     sig64 = schnorr_adapt(sig, t)
     negated_parity = b"\x02" if sig[0] == 3 else b"\x03"
     sig = negated_parity + sig[1:]
-    return (None, None, None, None, T, t, sig, sig64, "Adaptor extraction and Adapting", "FALSE", "Adaptor extraction and Adapting test: parity of R' is wrong")
+    return (t, sig, sig64, "FALSE", "Adaptor extraction and Adapting test: parity of R' is wrong")
 
 def vector11():
     t = 102737890522302903489175669941446530087577081774431208355392319064688794746007
@@ -167,48 +176,72 @@ def vector11():
     sig64 = schnorr_adapt(sig, t)
     R = bytes_from_int(0x9F08BE865F693BDBC1AE976DFFB8BC884C372052B0926E49E0298A9F2D7B4860)
     sig64 = R + sig64[32:64]
-    return (None, None, None, None, T, t, sig, sig64, "Adapting", "FALSE", "Adapting test: R value does not match")
+    return (t, sig, sig64, "FALSE", "Adapting test: R value does not match")
 
+presig_vectors = [
+    vector0(),
+    vector1(),
+    vector2(),
+    vector3(),
+    vector4(),
+    vector5(),
+    vector6(),
+    vector7(),
+]
 
-vectors = [
-        vector0(),
-        vector1(),
-        vector2(),
-        vector3(),
-        vector4(),
-        vector5(),
-        vector6(),
-        vector7(),
-        vector8(),
-        vector9(),
-        vector10(),
-        vector11(),
-    ]
+adapt_secadaptor_vectors = [
+    vector8(),
+    vector9(),
+    vector10(),
+    vector11(),
+]
 
-# Converts the byte strings of a test vector into hex strings
-def bytes_to_hex(seckey, pubkey, aux_rand, msg, T, t, sig, sig64, test_type, result, comment):
-    return (seckey.hex().upper() if seckey is not None else None, 
-            pubkey.hex().upper() if pubkey is not None else None,
-            aux_rand.hex().upper() if aux_rand is not None else None, 
-            msg.hex().upper() if msg is not None else None,
-            T.hex().upper() if T is not None else None, 
-            t.hex().upper() if t is not None else None,
-            sig.hex().upper(), 
-            sig64.hex().upper() if sig64 is not None else None,
-            test_type,
-            result,
-            comment)
+# Converts the byte strings of a presig vector into hex strings
+def presig_vector_to_hex(seckey, pubkey, aux_rand, msg, T, sig, result, comment):
+    return (
+        seckey.hex().upper() if seckey is not None else None,
+        pubkey.hex().upper(),
+        aux_rand.hex().upper() if aux_rand is not None else None,
+        msg.hex().upper(),
+        T.hex().upper(),
+        sig.hex().upper(),
+        result,
+        comment
+    )
 
-vectors = list(map(lambda vector: bytes_to_hex(vector[0], vector[1], vector[2], vector[3], vector[4], vector[5], vector[6], vector[7], vector[8], vector[9], vector[10]), vectors))
+# Converts the byte strings of a adapt-secadaptor vector into hex strings
+def adapt_secadaptor_vector_to_hex(t, sig, sig64, result, comment):
+    return (
+        t.hex().upper() if t is not None else None,
+        sig.hex().upper(),
+        sig64.hex().upper() if sig64 is not None else None,
+        result,
+        comment
+    )
 
-def print_csv(vectors):
+presig_vectors_hex = list(map(lambda vector: presig_vector_to_hex(vector[0], vector[1], vector[2], vector[3], vector[4], vector[5], vector[6], vector[7]), presig_vectors))
+
+adapt_secadaptor_vectors_hex = list(map(lambda vector: adapt_secadaptor_vector_to_hex(vector[0], vector[1], vector[2], vector[3], vector[4]), adapt_secadaptor_vectors))
+
+def print_csv(vectors, is_presig):
     writer = csv.writer(sys.stdout)
-    writer.writerow(("index", "secret key", "public key", "aux_rand", "message", "adaptor", "secadaptor", "pre-signature", "bip340 signature", "target api", "result", "comment"))
+    if is_presig:
+        writer.writerow(("index", "secret key", "public key", "aux_rand", "message", "adaptor", "pre-signature", "result", "comment"))
+    else:
+        writer.writerow(("index", "secadaptor", "pre-signature", "BIP340 signature", "result", "comment"))
     for (i,v) in enumerate(vectors):
         writer.writerow((i,)+v)
 
 if __name__ == "__main__":
-    output_file = "test_vectors.csv"
-    with open(output_file, "w") as f:
+    # Contains test vectors for `schnorr_presig_sign` and `schnorr_extract_adaptor` APIs
+    out1 = "presig_vectors.csv"
+    # Contains test vectors for `schnorr_adapt` and `schnorr_extract_secadaptor` APIs
+    out2 = "adapt_secadaptor_vectors.csv"
+
+    with open(out1, "w") as f:
         sys.stdout = f
-        print_csv(vectors)
+        print_csv(presig_vectors_hex, True)
+
+    with open(out2, "w") as f:
+        sys.stdout = f
+        print_csv(adapt_secadaptor_vectors_hex, False)
